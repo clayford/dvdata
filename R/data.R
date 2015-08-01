9bc3ed0af6651d87b29f89ccae671d586982a23c
Clay Ford <joe.clayton.ford@gmail.com>
2015-08-01 13:51:14 -0400

    fix save in get_dvdata

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Acid copper pattern plating experiment.
 #'
 #' Experiment to minimize coating thinkness variation of an acid copper-plating
-#' process. Chapter 16 (p. 558)
+#' process. (Chapter 16)
 #' @format A data frame with 10 rows and 5 variables:
 #' \describe{
 #'   \item{xA}{Anode-cathode separation (in.) - uncoded}
@@ -13,3 +13,19 @@
 #' @source \url{http://www.wright.edu/~dan.voss/bookdata/data.html}
 #'
 "acid.copper.pattern.plating"
+
+#' Aerosol
+#'
+#' Experiment to determine whether the current standard aerosol that was
+#' used for testing respirator filters could be replaced by an alternative
+#' aerosol. (Chapter 18)
+#' @format A data frame with 24 rows and 4 variables:
+#' \describe{
+#'   \item{MANUF}{Manufacturer}
+#'   \item{FILTER}{Filter type}
+#'   \item{AERO}{Aerosol type}
+#'   \item{Y}{Percent penetration}
+#' }
+#' @source \url{http://www.wright.edu/~dan.voss/bookdata/data.html}
+"aerosol"
+
--- data-raw/get_dvdata.R
+++ data-raw/get_dvdata.R
@@ -1,19 +1,18 @@
 # download and prepare data
-
 URL <- "http://www.wright.edu/~dan.voss/bookdata/data.html"
 web_page <- readLines(URL)
-lines <- grep(pattern = "\\.txt\">txt", web_page)
-flines <- web_page[lines] # lines that have URL to txt file
+flines <- web_page[grep(pattern = "\\.txt\">txt", web_page)]
 
 # extract URL to text files
 library(qdapRegex)
 links <- unlist(rm_between(flines, "\"", "\"",  extract=TRUE))
 links <- links[grep(pattern = "txt$",x = links)] # drop any .data files
+dfnames <- unlist(rm_between(links, "txt/", ".txt",  extract=TRUE))
+
 # create rdata files
 for(i in 1:length(links)){
-  tmp <- read.table(file = links[i], header=TRUE)
-  dname <- unlist(rm_between(links[i], "txt/", ".txt",  extract=TRUE))
-  save(tmp, file = paste0("data/",dname,".rdata"), compress = 'xz')
+  assign(x = dfnames[i], value = read.table(file = links[i], header=TRUE))
+  save(list=dfnames[i], file = paste0("data/",dfnames[i],".rdata"),
+       compress = 'xz')
 }
-
 
