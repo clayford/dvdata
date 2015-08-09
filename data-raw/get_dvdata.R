8f2c39700d8b4ee8fdc3629859bb85e391dc8432
Clay Ford <joe.clayton.ford@gmail.com>
2015-08-09 11:03:11 -0400

    set factors in data frames

--- data-raw/get_dvdata.R
+++ data-raw/get_dvdata.R
@@ -27,3 +27,36 @@ save(torque.optimization.2, file="data/torque.optimization.2.rdata")
 
 heartlung.pump$RPM <- factor(heartlung.pump$RPM)
 save(heartlung.pump, file="data/heartlung.pump.rdata")
+
+aerosol[,1:3] <- lapply(aerosol[,1:3],factor)
+save(aerosol, file="data/aerosol.rdata")
+
+air.freshener[,1:3] <- lapply(air.freshener[,1:3],factor)
+save(air.freshener, file="data/air.freshener.rdata")
+
+air.rifle[,1:3] <- lapply(air.rifle[,1:3],factor)
+save(air.rifle, file="data/air.rifle.rdata")
+
+air.velocity[,1:3] <- lapply(air.velocity[,1:3],factor)
+save(air.velocity, file="data/air.velocity.rdata")
+
+alcohol$BOTTLE <- factor(alcohol$BOTTLE)
+save(alcohol, file="data/alcohol.rdata")
+
+algorithm[,c(1:4,6)] <- lapply(algorithm[,c(1:4,6)],factor)
+save(algorithm, file="data/algorithm.rdata")
+
+ammunition[,1:2] <- lapply(ammunition[,1:2],factor)
+save(ammunition, file="data/ammunition.rdata")
+
+balloon$COLOR <- factor(balloon$COLOR)
+save(balloon, file="data/balloon.rdata")
+
+banana[,1:3] <- lapply(banana[,1:3],factor)
+save(banana, file="data/banana.rdata")
+
+battery$TYPEBAT <- factor(battery$TYPEBAT)
+save(battery, file="data/battery.rdata")
+
+bean.soaking$TIME <- factor(bean.soaking$TIME)
+save(bean.soaking,file="data/bean.soaking.rdata")
